90ed0589d50c7a35dd2d579292ebad65595ff424
Евгений Чернецкий <Anidalis1@yandex.ru>
2023-03-26 17:53:49 +0300

    Init commit

---
+++ src/main/java/ru/coulthard/webpass/WebpassApplication.java
@@ -0,0 +1,13 @@
+package ru.coulthard.webpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebpassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebpassApplication.class, args);
+    }
+
+}
